64c711d78e8127e0a7b52863063f03616b2e22ad
denebw <denebwang@outlook.com>
2021-05-04 15:55:53 +0800

    clean up

--- Soft-landing.py
+++ Soft-landing.py
@@ -4,6 +4,7 @@ import json
 import numpy as np
 from scipy import optimize
 import UPG
+from Utilities import target_reference_frame
 
 conn = krpc.connect()
 print("krpc connected")
@@ -13,9 +14,8 @@ ap = vessel.auto_pilot
 body = vessel.orbit.body
 body_frame = body.reference_frame
 flight = vessel.flight(body_frame)
-# multipliers to normalize variables
+
 upg = UPG.UPG(conn)
-# soft landing prob with 3-arc thrust
 
 # load params
 with open("target.json") as f:
@@ -28,15 +28,8 @@ min_throttle = params['min_throttle']
 r_f = np.asarray(body.position_at_altitude(lat, lon,
                                            body.surface_height(lat, lon) + 250, body_frame)).reshape(-1, 1)
 # get final velocity in body frame
-temp1 = space_center.ReferenceFrame.create_relative(body_frame,
-                                                    rotation=(0., np.sin(-lon / 2. * np.pi / 180), 0.,
-                                                              np.cos(-lon / 2. * np.pi / 180)))
-temp2 = space_center.ReferenceFrame.create_relative(temp1,
-                                                    rotation=(0., 0., np.sin(lat / 2. * np.pi / 180),
-                                                              np.cos(lat / 2. * np.pi / 180)))
-height = body.surface_height(lat, lon) + body.equatorial_radius
-target_frame = space_center.ReferenceFrame.create_relative(temp2, position=(height, 0., 0.))
-del temp1, temp2
+
+target_frame = target_reference_frame(space_center, body, lat, lon)
 target_v = np.array([-v, 0., 0.])
 v_f = np.asarray(space_center.transform_velocity((0., 0., 0.), target_v, target_frame, body_frame)).reshape(-1, 1)
 t1 = 15.
@@ -52,112 +45,74 @@ current_thrust = conn.add_stream(getattr, vessel, 'thrust')
 mass = conn.add_stream(getattr, vessel, 'mass')
 ut = conn.add_stream(getattr, space_center, 'ut')
 height = conn.add_stream(getattr, flight, 'surface_altitude')
-
-# get initial values
 velocity = conn.add_stream(vessel.velocity, body_frame)
 position = conn.add_stream(vessel.position, body_frame)
+
+# get initial values
 thrust = max_thrust()
 m0 = mass()
 specific_impulse = vacuum_specific_impulse()
 v_0 = np.asarray(velocity()).reshape(-1, 1) / upg.velocity_multiplier
 r_0 = np.asarray(position()).reshape(-1, 1) / upg.position_multiplier
+x = np.vstack((r_0, v_0))
 t_0 = ut() / upg.time_multiplier
 t_1 = t_0 + t1 / upg.time_multiplier
 
 # initial guess
-
 pv = np.asarray(-v_0 / np.linalg.norm(v_0)).reshape(-1, 1)
 pr = np.zeros((3, 1))
 tf = np.array([(ut() + 350.) / upg.time_multiplier]).reshape(1, 1)
-x = np.vstack((r_0, v_0))
-
 z0 = np.hstack((pv.T, pr.T, tf)).T
 
-
-def fun(z, x_0, t_0, t_1, t_2, thrust, min_t, isp, m0):
-    pv = z[0:3].reshape(-1, 1)
-    pr = z[3:6].reshape(-1, 1)
-    tf = z[6]
-    # final position constraint
-    x_f = upg.x_t(x_0, tf, t_0, t_1, t_2, pv, pr, thrust, min_t, isp, m0)
-    r_tf = x_f[0:3, :]
-    s1 = r_tf.T @ r_tf - r_f.T @ r_f
-    # final velocity constraint
-    v_tf = x_f[3:6, :]
-    s2 = v_tf - v_f  # s2 includes 3 equations
-    # transversality condition
-    pvf, prf = upg.lambda_t(tf, t_0, pv, pr)
-
-    s3 = prf.T @ v_tf - pvf.T @ r_tf + \
-         np.linalg.norm(pvf) * thrust / (upg.mass_t(tf, t_0, specific_impulse, m0, thrust) * upg.g0) - 1
-    s4 = r_tf[2, 0] * prf[0, 0] - r_tf[0, 0] * prf[2, 0]
-    s5 = r_tf[2, 0] * prf[1, 0] - r_tf[1, 0] * prf[2, 0]
-    return [s1[0, 0], s2[0, 0], s2[1, 0], s2[2, 0], s3[0, 0], s4, s5]
-
-
-def fun_2(t2, z0, x_0, t_0, t_1, thrust, min_t, isp, m0):
-    sol = optimize.root(fun, z0, args=(x_0, t_0, t_1, t2, thrust, min_t, isp, m0), method='lm', jac=False)
-    z = sol.x.reshape(-1, 1)
-    tf = z[6, 0]
-    m_t1 = upg.mass_t(t_1, t_0, isp, m0, thrust)
-    m_t2 = upg.mass_t(t2, t_1, isp, m_t1, thrust * min_t)
-    m_tf = upg.mass_t(tf, t2, isp, m_t2, thrust)
-    return m0 - m_tf
-
-
-print("evaluating t2")
-result = optimize.minimize_scalar(fun_2, bounds=(t_1, tf[0, 0]),
-                                  args=(z0, x, t_0, t_1, thrust, min_throttle, specific_impulse, m0), method='Bounded')
-t_2 = result.x
+print("evaluating t2...")
+t_2 = optimize.minimize_scalar(
+    upg.target_function_t2_sl, bounds=(t_1, tf[0, 0]),
+    args=(z0, r_f, v_f, x, t_0, t_1, thrust, min_throttle, specific_impulse, m0), method='Bounded').x
 # convert to time interval
 t2 = t_2 - t_0
-print("optimal t2:" + str(t2 * upg.time_multiplier))
-ap.reference_frame = body_frame
-ap.engage()
+print("optimal t2: {:.2f}".format(t2 * upg.time_multiplier))
 
 # run once
 print("initiating UPG")
-sol = optimize.root(fun, z0, args=(x, t_0, t_1, t_2, thrust, min_throttle, specific_impulse, m0),
+sol = optimize.root(upg.target_function_sl, z0,
+                    args=(r_f, v_f, x, t_0, t_1, t_2, thrust, min_throttle, specific_impulse, m0),
                     method='lm', jac=False)
 z = sol.x.reshape(-1, 1)
 z0 = z
 pv = z[0:3, :]
-pr = z[3:6, :]
-tf = z[6, 0]
 unit_t = pv / np.linalg.norm(pv)
-ap.target_direction = tuple(unit_t.reshape(1, -1)[0])
+
+ap.reference_frame = body_frame
+ap.engage()
+ap.target_direction = tuple(unit_t.flatten())
 print("pointing to correct direction")
 ap.wait()
 
-v_0 = np.asarray(velocity()).reshape(-1, 1) / upg.velocity_multiplier
-r_0 = np.asarray(position()).reshape(-1, 1) / upg.position_multiplier
-x = np.vstack((r_0, v_0))
-m0 = mass()
-thrust = max_thrust()
-t_0 = ut() / upg.time_multiplier
+x, m0, thrust, t_0 = upg.update(velocity, position, mass, max_thrust, ut)
 t_1 = t_0 + t1 / upg.time_multiplier
 t_2 = t_0 + t2
 
 vessel.control.throttle = 1
 while True:
-    sol = optimize.root(fun, z0, args=(x, t_0, t_1, t_2, thrust, min_throttle, specific_impulse, m0),
-                        method='lm', jac=False)
+    sol = optimize.root(
+        upg.target_function_sl, z0,
+        args=(r_f, v_f, x, t_0, t_1, t_2, thrust, min_throttle, specific_impulse, m0), method='lm', jac=False)
     z = sol.x.reshape(-1, 1)
     z0 = z
     pv = z[0:3, :]
-    pr = z[3:6, :]
     tf = z[6, 0]
     tgo = (tf - t_0) * upg.time_multiplier
     tgo_1 = (t_1 - t_0) * upg.time_multiplier
     tgo_2 = (t_2 - t_0) * upg.time_multiplier
     distance = np.linalg.norm((r_0 - r_f) * upg.position_multiplier)
-
+    v_go = upg.v_go(specific_impulse, m0)
     print("tgo: {:.2f}".format(tgo))
     if tgo_1 > 0:
         print("tgo1: {:.2f}".format(tgo_1))
     if tgo_2 > 0:
         print("tgo2: {:.2f}".format(tgo_2))
     print("distance: {:.2f}".format(distance))
+    print("vgo: {:.2f}".format(v_go))
     unit_t = pv / np.linalg.norm(pv)
     ap.target_direction = tuple(unit_t.flatten())
     if t_1 < t_0 < t_2:
@@ -170,7 +125,7 @@ while True:
     time.sleep(0.2)
 
     # correct target to the correct height
-    x_f = upg.x_t(x, tf, t_0, t_1, t_2, pv, pr, thrust, min_throttle, specific_impulse, m0)
+    x_f = upg.x_f
     r_f_t = tuple(upg.position_multiplier * x_f[0:3, :].flatten())
     lat = body.latitude_at_position(r_f_t, body_frame)
     lon = body.longitude_at_position(r_f_t, body_frame)
@@ -178,28 +133,21 @@ while True:
         lat, lon, body.surface_height(lat, lon) + 300, body_frame)).reshape(-1, 1) / upg.position_multiplier
 
     # get new values for next loop
-    v_0 = np.asarray(velocity()).reshape(-1, 1) / upg.velocity_multiplier
-    r_0 = np.asarray(position()).reshape(-1, 1) / upg.position_multiplier
-    x = np.vstack((r_0, v_0))
-    m0 = mass()
-    thrust = max_thrust()
-    t_0 = ut() / upg.time_multiplier
-
-# vessel.control.throttle = 0.
-
+    x, m0, thrust, t_0 = upg.update(velocity, position, mass, max_thrust, ut)
+# ADGP
 # change r_f to ground
 r_f = np.asarray(body.position_at_altitude(
     lat, lon, body.surface_height(lat, lon) + 10, body_frame)).reshape(-1, 1)
-# ADGP
+
 a_f = np.zeros((3, 1))
 v_f = np.zeros((3, 1))
 # calculate tgo
 sin_gamma = flight.vertical_speed / flight.speed
 
 
-def fun_at(a_t, v_0, s_gamma, h0, g0):
-    return a_t ** 2 + ((v_0 ** 2 * s_gamma) / (2 * h0)) * a_t - \
-           (v_0 ** 2 * g0 * (1 + s_gamma ** 2) / (4 * h0)) - g0 ** 2
+def fun_at(at, v0, s_gamma, h0, g0):
+    return at ** 2 + ((v0 ** 2 * s_gamma) / (2 * h0)) * at - \
+           (v0 ** 2 * g0 * (1 + s_gamma ** 2) / (4 * h0)) - g0 ** 2
 
 
 V_0 = flight.speed
@@ -211,13 +159,12 @@ print('tgo:' + str(t_f))
 t_f = t_f + ut()
 print("Terminal guidance engaged")
 while True:
-    time.sleep(0.05)
+    time.sleep(0.1)
 
     v_0 = np.asarray(velocity()).reshape(-1, 1)
     r_0 = np.asarray(position()).reshape(-1, 1)
     m0 = mass()
     thrust = max_thrust()
-    curr_t = current_thrust()
     t_0 = ut()
     t_go = t_f - t_0
     print("t_go: {:.2f}".format(t_go))
@@ -232,8 +179,9 @@ while True:
     vessel.control.throttle = throttle
     unit_t = a_t / a
     ap.target_direction = tuple(unit_t.flatten())
-    if tgo < 0.5 or r_go < 5 or height() < 6:
+    if tgo < 0.5 or r_go < 5 or height() < 5:
         print("ADGP disengaged")
+        vessel.control.throttle = 0.
         break
 while True:
     if vessel.situation == space_center.VesselSituation.landed \
@@ -241,7 +189,5 @@ while True:
         print("landed!")
         break
 ap.disengage()
-vessel.control.throttle = 0.
 vessel.control.rcs = False
-print("Landing precision: {:.2f}m".format(distance))
-
+print("Landing precision: {:.2f}m".format(r_go))
--- UPG.py
+++ UPG.py
@@ -1,6 +1,9 @@
 import numpy as np
+from scipy.optimize import root
 
 ge = 9.80665  # standard gravity which is used to calculate exhaust velocity
+
+
 
 
 class UPG(object):
@@ -19,6 +22,9 @@ class UPG(object):
         self.time_multiplier = np.sqrt(self.r0 / self.g0)
         self.position_multiplier = self.r0
         self.velocity_multiplier = np.sqrt(self.r0 * self.g0)
+        # stored in class to avoid duplicate calculation
+        self.x_f = np.zeros((6, 1))
+        self.m_f = 0
 
     def mass_t(self, t, t0, isp, m0, thrust):
         """
@@ -81,6 +87,7 @@ class UPG(object):
         :param m0: initial mass
         :return: the corresponding satate vector
         """
+
         def i(t, t0, pv, pr, thrust, isp, m0):
             i_c = self.thrust_integral('c', t, t0, pv, pr, thrust, isp, m0)
             i_s = self.thrust_integral('s', t, t0, pv, pr, thrust, isp, m0)
@@ -102,6 +109,7 @@ class UPG(object):
             m_t2 = self.mass_t(t2, t1, isp, m_t1, thrust * min_t)
             # x at t
             i_t = i(t, t2, pv_t2, pr_t2, thrust, isp, m_t2)
+            self.m_f = self.mass_t(t, t2, isp, m_t2, thrust)
             return self.transfer_mat(t, t2) @ x_temp + self.gamma_mat(t, t2) @ i_t
         elif t0 > t1:
             if t0 < t2:
@@ -112,9 +120,11 @@ class UPG(object):
                 x_temp = self.transfer_mat(t2, t0) @ x0 + self.gamma_mat(t2, t0) @ i_t2
                 # x at t
                 i_t = i(t, t2, pv_t2, pr_t2, thrust, isp, m_t2)
+                self.m_f = self.mass_t(t, t2, isp, m_t2, thrust)
                 return self.transfer_mat(t, t2) @ x_temp + self.gamma_mat(t, t2) @ i_t
             elif t0 > t2:
                 i_t = i(t, t0, pv0, pr0, thrust, isp, m0)
+                self.m_f = self.mass_t(t, t0, isp, m0, thrust)
                 return self.transfer_mat(t, t0) @ x0 + self.gamma_mat(t, t0) @ i_t
 
     def thrust_integral(self, type_name, t, t0, pv0, pr0, thrust, isp, m0):
@@ -145,3 +155,48 @@ class UPG(object):
         delta = (t - t0) / 4.
         return ((t - t0) / 90.) * (7 * i(t0) + 32 * i(t0 + delta) + 12 * i(t0 + 2 * delta) + 32 * i(t0 + 3 * delta) +
                                    7 * i(t0 + 4 * delta))
+
+    def target_function_sl(self, z, r_f, v_f, x_0, t_0, t_1, t_2, thrust, min_t, isp, m0):
+        pv = z[0:3].reshape(-1, 1)
+        pr = z[3:6].reshape(-1, 1)
+        tf = z[6]
+        # final position constraint
+        self.x_f = self.x_t(x_0, tf, t_0, t_1, t_2, pv, pr, thrust, min_t, isp, m0)
+        r_tf = self.x_f[0:3, :]
+        s1 = r_tf.T @ r_tf - r_f.T @ r_f
+        # final velocity constraint
+        v_tf = self.x_f[3:6, :]
+        s2 = v_tf - v_f  # s2 includes 3 equations
+        # transversality condition
+        pvf, prf = self.lambda_t(tf, t_0, pv, pr)
+
+        s3 = prf.T @ v_tf - pvf.T @ r_tf + \
+             np.linalg.norm(pvf) * thrust / (self.mass_t(tf, t_0, isp, m0, thrust) * self.g0) - 1
+        s4 = r_tf[2, 0] * prf[0, 0] - r_tf[0, 0] * prf[2, 0]
+        s5 = r_tf[2, 0] * prf[1, 0] - r_tf[1, 0] * prf[2, 0]
+        return [s1[0, 0], s2[0, 0], s2[1, 0], s2[2, 0], s3[0, 0], s4, s5]
+
+    def target_function_t2_sl(self, t2, z0, r_f, v_f, x_0, t_0, t_1, thrust, min_t, isp, m0):
+        # m_f is calculated during root finding
+        root(
+            self.target_function_sl, z0, args=(r_f, v_f, x_0, t_0, t_1, t2, thrust, min_t, isp, m0),
+            method='lm', jac=False)
+        '''z = sol.x.reshape(-1, 1)
+        tf = z[6, 0]
+        m_t1 = self.mass_t(t_1, t_0, isp, m0, thrust)
+        m_t2 = self.mass_t(t2, t_1, isp, m_t1, thrust * min_t)
+        m_tf = self.mass_t(tf, t2, isp, m_t2, thrust)'''
+        return m0 - self.m_f
+
+    def v_go(self, isp, m0):
+        v_exh = isp * ge
+        return v_exh*np.log(m0/self.m_f)
+
+    def update(self, velocity_stream, position_stream, mass_stream, max_thrust_stream, ut_stream):
+        v_0 = np.asarray(velocity_stream()).reshape(-1, 1) / self.velocity_multiplier
+        r_0 = np.asarray(position_stream()).reshape(-1, 1) / self.position_multiplier
+        x = np.vstack((r_0, v_0))
+        m0 = mass_stream()
+        thrust = max_thrust_stream()
+        t_0 = ut_stream() / self.time_multiplier
+        return x, m0, thrust, t_0
